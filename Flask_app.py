1b0d8dea3610e777800773a1834a6a143f32bd50
Marco308 <46830750+marco308@users.noreply.github.com>
2020-05-28 18:43:55 +0100

    super basic flask web app

---
+++ Flask_app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request #render_template used to all templates 
+
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 #prevent caching so changes flow
+
+
+@app.route('/')
+def index():
+    return render_template('/index.html')
+
+
+##################
+##################
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5003) 
+    
+    #ssl_context='adhoc') 
+    
